651bb566e7724cbd15af3fd2ceda640a4bf2f381
Fanirisoa <fanirisoa.zazaravaka@gmail.com>
2020-07-14 23:30:42 +0200

    update new version

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 #! /usr/bin/python/  python3
 # coding: utf-8
 
+import math
+import os
+import random
+import re
+import sys
 
-if __name__ == "__main__":
-    main()
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+
+    strings_count = int(input())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+
