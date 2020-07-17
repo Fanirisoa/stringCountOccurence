7df08593e1e55e5c0e673a0d28ec45347f2fa127
Fanirisoa <fanirisoa.zazaravaka@gmail.com>
2020-07-17 05:27:19 +0200

    update new version

--- sparseArray.py
+++ sparseArray.py
@@ -2,24 +2,58 @@
 # coding: utf-8
 
 class setStringToCompare:
+    """
+    A class used to represent a collection of query strings and a collection of input strings
 
+    ...
+
+    Attributes
+    ----------
+    queries : array[str]
+        an array of query strings
+    strings : array[str]
+       an array of strings to search
+
+    Methods
+    -------
+    says(sound=None)
+        Prints the animals name and what sound it makes
+    """
 
 	# définition de la méthode spéciale __init__
     def __init__(self, queries, strings):
-        # deux paramètres supplémentaires
+        """
+        a constructor to initialize the attributes of the class setStringToCompare.
+
+        : queries : array[str]  an array of query strings
+        : strings : array[str]  an array of strings to search
+        """
         self.queries = queries
         self.strings = strings
 
 
-    # matchingStringsToVec function: Compare queries and strings and return a vector of occurence.
+    
     def matchingStringsToVec(self):
+       """ 
+       Customize function matchingStringsToVec : Compare queries and strings and return  an integer array of occurence.
+        
+        : param e : the column
+        : return  : an array of integers representing the frequency of occurrence of each query string in strings.
+       """
         countList = []
         for _ in self.queries:
             countList.append(self.strings.count(_))
         return countList   
 
-    # matchingStringsToDic function: Compare queries and strings and return a vector of occurence.
-    def matchingStringsToDic(self):    
+    def matchingStringsToDic(self):
+       """ 
+       Customize function matchingStringsToDic : Compare queries and strings and return a dictionary of occurence where 
+            - key[i] : queries[i]  ith element of queries
+            - value[i] : the frequency of occurrence of queries[i] in strings
+        
+        : param e : the column
+        : return  : an array of integers representing the frequency of occurrence of each query string in strings.
+       """    
         return dict((x,self.strings.count(x)) for x in set(self.queries))
 
 
